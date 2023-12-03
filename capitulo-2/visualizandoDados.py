cc4954b26828dac379f20f84f4371f713f3071a4
gabriel <gabriel.exposto1357@gmail.com>
2023-12-03 05:09:08 -0300

    co-relacionando dados para uma melhor visualização

--- capitulo-2/visualizandoDados.py
+++ capitulo-2/visualizandoDados.py
@@ -1,10 +1,16 @@
 # objetivo do codigo a seguir e explorar e visualizar os dados do banco de dados
 from conjuntoTeste import *
 import pandas as pd
+from pandas.plotting import scatter_matrix
 import numpy as np 
 import matplotlib.pyplot as plt
 
 housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4, 
              s=housing["population"]/100, label="population", figsize=(10,7),
              c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True)
+
+corr_matrix=housing.corr(numeric_only=True)
+print(corr_matrix["median_house_value"].sort_values(ascending=False))
+attributes = ["median_house_value", "median_income", "total_rooms", "housing_median_age"]
+scatter_matrix(housing[attributes], figsize=(12,8))
 plt.show()
